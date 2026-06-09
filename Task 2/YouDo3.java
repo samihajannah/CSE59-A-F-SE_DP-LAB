68fea35962bfe9d2e34420cbf556fb0367cdd5e4
samihajannah <samihajannahnajah01@gmail.com>
2026-06-09 10:48:22 +0600

    Update YouDo3.java

--- Task 2/YouDo3.java
+++ Task 2/YouDo3.java
@@ -1,1 +1,16 @@
 
+class Countdown {
+    void start() {
+        for (int i = 5; i >= 1; i--){
+            System.out.println(i);
+        }
+        System.out.println("Go!");
+    }
+}
+
+public class main3 {
+    public static void main(String[]args){
+        Countdown obj = new Countdown();
+        obj.start();
+    }
+}
