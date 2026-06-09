a7f5160e5d7afc6b81fe92db95a457a18bfdce1b
samihajannah <samihajannahnajah01@gmail.com>
2026-06-09 10:09:46 +0600

    Update YouDo1.java

--- Task 2/YouDo1.java
+++ Task 2/YouDo1.java
@@ -1,1 +1,15 @@
 
+class ContestStats {
+    int[] scores = {85, 90, 95};
+
+    void printFirstScore() {
+        System.out.println("First score: " + scores[0]);
+    }
+}
+
+public class main1 {
+    public static void main(String[] args) {
+        ContestStats obj = new ContestStats();
+        obj.printFirstScore();
+    }
+}
